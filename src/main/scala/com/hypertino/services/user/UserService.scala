f6237baf935664de2f1d765842b4dee4a208f2aa
Maga Abdurakhmanov <maqdev@gmail.com>
2017-07-17 04:35:50 +0200

    WIP: working on user creation

--- src/main/scala/com/hypertino/services/user/UserService.scala
+++ src/main/scala/com/hypertino/services/user/UserService.scala
@@ -1,8 +1,8 @@
 package com.hypertino.services.user
 
-import com.hypertino.binders.value.Lst
+import com.hypertino.binders.value.{Lst, Null, Obj, Value}
 import com.hypertino.hyperbus.Hyperbus
-import com.hypertino.hyperbus.model.{DynamicBody, ErrorBody, NotFound, Ok, ResponseBase}
+import com.hypertino.hyperbus.model.{BadRequest, Conflict, DynamicBody, ErrorBody, MessagingContext, NotFound, Ok, ResponseBase}
 import com.hypertino.hyperbus.serialization.SerializationOptions
 import com.hypertino.service.control.api.Service
 import com.hypertino.user.api.{UserGet, UserPatch, UsersGet, UsersPost}
@@ -18,7 +18,7 @@ class UserService (implicit val injector: Injector) extends Service with Injecta
   private val log = LoggerFactory.getLogger(getClass)
   private implicit val scheduler = inject[Scheduler]
   private val hyperbus = inject[Hyperbus]
-  private val config = UserServiceConfiguration(Map("user_id" → None, "email" → None))
+  private val config = UserServiceConfiguration(Map("email" → None))
   private implicit val so = SerializationOptions.forceOptionalFields
   private val handlers = hyperbus.subscribe(this, log)
 
@@ -28,32 +28,48 @@ class UserService (implicit val injector: Injector) extends Service with Injecta
     if (request.headers.hrl.query.user_id.isDefined) {
       wrapIntoCollection(request.headers.hrl.query.user_id.toString)
     } else {
-      val identityKey = request.headers.hrl.query.toMap.find { kv ⇒
-        kv._1 != "user_id" && config.keyFields.contains(kv._1)
-      }
-      identityKey.map { ik ⇒
-        hyperbus
-          .ask(ContentGet(hyperStorageUserPathByIdentityKey(ik._1, ik._2.toString)))
-          .map { ok ⇒
-            wrapIntoCollection(ok.body.content.user_id.toString)
-          }
-          .flatten
-      } getOrElse {
-        Task.eval(NotFound(ErrorBody("identity-key-not-specified", Some("Can't lookup all users"))))
-      }
+      identityFields(request.headers.hrl.query)
+        .headOption
+        .map { identityKey ⇒
+          getUserByIdentityKey(identityKey)
+            .map { optionUserId ⇒
+              optionUserId.map(wrapIntoCollection)
+            }
+        }
+        .flatten
+        .getOrElse {
+          Task.eval(NotFound(ErrorBody("identity-key-not-specified", Some("Can't lookup all users"))))
+        }
     }
   }
 
   def onUsersPost(implicit request: UsersPost): Task[ResponseBase] = {
+    if (request.body.content.user_id.isDefined) {
+      Task.raiseError(BadRequest(ErrorBody("user-id-prohibited", Some("You can't set user_id explicitly"))))
+    }
+    else {
+      import com.hypertino.binders.value._
+      getUsersByIdentityKeys(identityFields(request.body.content)).flatMap { existingUsers ⇒
+        if (existingUsers.nonEmpty) {
+          Task.raiseError(Conflict(ErrorBody("duplicate-identity-keys", Some("There is an existing user with identity keys"), extra = existingUsers.toValue)))
+        }
+        else {
+          // todo: we need here a transaction manager
+
+
+
+        }
+      }
+    }
+
     //  1. check if exists with the identity-key's
     //  2. insert identity-key -> user_id
     //  3. insert user
-    ???
   }
 
   def onUserGet(implicit request: UserGet): Task[Ok[DynamicBody]] = {
-    hyperbus
-      .ask(ContentGet(hyperStorageUserPath(request.userId)))
+    getUserByUserId(request.userId)
+      .map(user ⇒ Ok(DynamicBody(user)))
   }
 
   def onUserPatch(implicit request: UserPatch): Task[ResponseBase] = {
@@ -64,12 +80,48 @@ class UserService (implicit val injector: Injector) extends Service with Injecta
     ???
   }
 
-  private def wrapIntoCollection(userId: String): Task[ResponseBase] = {
-    onUserGet(UserGet(userId)).map { ok ⇒
-      Ok(DynamicBody(Lst.from(ok.body.content)))
-    } onErrorRecover {
-      case _: NotFound[_] ⇒ Ok(DynamicBody(Lst.empty))
+  private def wrapIntoCollection(userId: String)(implicit mcx: MessagingContext): Task[ResponseBase] = {
+    getUserByUserId(userId).map {
+      case Null ⇒ NotFound(ErrorBody("resource-not-found", Some(s"User $userId is not found")))
+      case user: Obj ⇒ Ok(DynamicBody(Lst.from(user)))
     }
+  }
+
+  private def identityFields(obj: Value): Map[String, Value] = {
+    obj.toMap.filter(kv ⇒ config.keyFields.contains(kv._1)).toMap
+  }
+
+  private def getUserByUserId(userId: String): Task[Value] = {
+    hyperbus
+      .ask(ContentGet(hyperStorageUserPath(userId)))
+      .map(_.body.content)
+      .onErrorRecover {
+        case _: NotFound[_] ⇒ Null
+      }
+  }
+
+  private def getUsersByIdentityKeys(identityKeys: Map[String, Value]): Task[Map[String, String]] = {
+    Task.gatherUnordered {
+      identityKeys.map { case (identityKeyType, identityKey) ⇒
+        hyperbus
+          .ask(ContentGet(hyperStorageUserPathByIdentityKey(identityKeyType, identityKey.toString)))
+          .map { ok ⇒
+            identityKeyType → ok.body.content.user_id.toString
+          }
+      }
+    } map(_.toMap)
+  }
+
+  private def getUserByIdentityKey(identityKey: (String, Value)): Task[Option[String]] = {
+    hyperbus
+      .ask(ContentGet(hyperStorageUserPathByIdentityKey(identityKey._1, identityKey._2.toString)))
+      .map { ok ⇒
+        Some(ok.body.content.user_id.toString)
+      }
+      .onErrorRecover {
+        case _: NotFound[_] ⇒
+          None
+      }
   }
 
   private def hyperStorageUserPath(userId: String): String = s"/services/user/users/{$userId}"
